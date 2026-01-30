0bb1164fdbc62bd615e7e0fd256385a3898a0d45
Sajiya14 <sajithperera215@gmail.com>
2026-01-30 22:34:23 +0530

    change main jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './Context/AuthContext';
+import { CartProvider } from './Context/CartContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <AuthContextProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
